81a65aac2b1686224394c8892f86f2dbb166aafd
Omar Fareed <119762537+omarfareed64@users.noreply.github.com>
2025-11-14 00:16:13 +0200

    solved problem when you press login and signup pages

--- src/app/Pages/login-page/login-page.ts
+++ src/app/Pages/login-page/login-page.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
-import { StrongPasswordRegx } from './regex';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { StrongPasswordRegx } from '../regex';
 
 @Component({
   selector: 'app-login-page',
@@ -12,32 +12,16 @@ import { StrongPasswordRegx } from './regex';
 export class LoginPage {
   private formBuilder = inject(FormBuilder);
 
-  signupForm = this.formBuilder.group(
-    {
-      userName: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.pattern(StrongPasswordRegx), Validators.minLength(6)]],
-      retypePassword: ['', Validators.required],
-    },
-    { validators: [LoginPage.passwordsMatchValidator] } // attach the validator
-  );
-
-  // Validator to check equality of password fields only
-  static passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
-    const password = control.get('password')?.value;
-    const retype = control.get('retypePassword')?.value;
-
-    if (password !== retype) {
-      return { passwordMismatch: true };
-    }
-    return null;
-  }
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(StrongPasswordRegx), Validators.minLength(6)]],
+  });
 
   onSubmit() {
-    if (this.signupForm.valid) {
-      console.log('Form submitted:', this.signupForm.value);
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
     } else {
-      console.log('Form invalid:', this.signupForm.errors);
+      console.log('Form invalid:', this.loginForm.errors);
     }
   }
 }
--- src/app/Pages/sign-up-page/sign-up-page.ts
+++ src/app/Pages/sign-up-page/sign-up-page.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { StrongPasswordRegx } from '../regex';
 
 @Component({
   selector: 'app-sign-up-page',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  
   templateUrl: './sign-up-page.html',
   styleUrl: './sign-up-page.css',
 })
 export class SignUpPage {
+private formBuilder = inject(FormBuilder);
+
+  signupForm = this.formBuilder.group(
+    {
+      userName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(StrongPasswordRegx), Validators.minLength(6)]],
+      retypePassword: ['', Validators.required],
+    },
+    { validators: [SignUpPage.passwordsMatchValidator] } // attach the validator
+  );
+
+  // Validator to check equality of password fields only
+  static passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const retype = control.get('retypePassword')?.value;
+
+    if (password !== retype) {
+      return { passwordMismatch: true };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log('Form submitted:', this.signupForm.value);
+    } else {
+      console.log('Form invalid:', this.signupForm.errors);
+    }
+  }
+
 
 }
+
+
