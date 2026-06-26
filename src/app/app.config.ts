79226a952f0465718a7ac493e64aafa1c4af6e79
Omar Fareed <119762537+omarfareed64@users.noreply.github.com>
2026-06-26 23:22:57 +0300

    Fix GitHub Pages deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
   ]
 };
